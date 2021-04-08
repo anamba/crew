8d3668e8676bbb06c5a287ce7bbdc590683e5f4f
Aaron Namba <aaron@aaronnamba.com>
2021-04-08 13:32:00 +0900

    updated to tailwind 2.1 w/jit

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+// const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -60,6 +60,6 @@ module.exports = (env, options) => {
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
-      .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
+    // .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
   }
 };
